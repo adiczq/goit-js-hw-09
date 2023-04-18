0e24244a42e8c4b78b686f948621b0dfb6e56862
Adrian Lacheta <adiczq@gmail.com>
2023-04-18 20:57:25 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,31 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+const formEl = document.querySelector('.form');
+console.log(formEl);
+formEl.addEventListener('submit', e => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const firstDelay = parseInt(formData.get('delay'), 10);
+  const step = parseInt(formData.get('step'), 10);
+  const amount = parseInt(formData.get('amount'), 10);
+
+  for (let i = 0; i < amount; i++) {
+    createPromise(i + 1, firstDelay + i * step)
+      .then(({ position, delay }) => {
+        console.log(`Fulfilled promise ${position} in ${delay} ms `);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`Rejected promise ${position} in ${delay} ms `);
+      });
+  }
+});
